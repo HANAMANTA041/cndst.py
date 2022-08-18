fd9e032b7b8b6f879bf01d279d4bcea23d3c5fa9
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-18 10:55:48 +0530

    cndst.md
    Different conditional statements of the python

---
+++ cndst.py
@@ -0,0 +1,30 @@
+#Conditional statements:They state a certain condition
+#a==b
+#a != b
+#a <b
+#a<=b
+#a>b
+#a >= b
+"""a = 33
+b = 200
+if a>b:
+    print("a is greater than b")
+else:print("b is greater than a")
+a = 33
+b = 33
+if a>b:
+    print("a is greater than b")
+elif a==b:# if the previous condition is not true execute
+    print("a and b are equal")
+a = 33
+b = 100
+if a < b:print("a is greater than b")
+print("a is greater than b") if a>b else print("b is greater than is")"""
+a = 333
+b = 200
+c = 500
+if a>b and c>a:#if we use or prints a
+    print("a")
+else:
+    print("b")
+
